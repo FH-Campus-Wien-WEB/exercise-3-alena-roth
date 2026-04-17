7a9d605b1acbff8c5143eb60928acd7b5a8bb9d5
alena-roth <alena.roth2@gmail.com>
2026-04-17 12:12:04 +0200

    1.1 done + own movies put back into movie-model

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const movieModel = require('./movie-model.js');
 const app = express();
 
 // Parse urlencoded bodies
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
@@ -15,6 +15,8 @@ app.use(express.static(path.join(__dirname, 'files')));
    This endpoint returns a sorted array of all the genres of the movies
    that are currently in the movie model.
 */
+
+
 
 /* Task 1.4: Extend the GET /movies endpoint:
    When a query parameter for a specific genre is given, 
@@ -29,28 +31,28 @@ app.get('/movies', function (req, res) {
 app.get('/movies/:imdbID', function (req, res) {
   const id = req.params.imdbID
   const exists = id in movieModel
- 
+
   if (exists) {
     res.send(movieModel[id])
   } else {
-    res.sendStatus(404)    
+    res.sendStatus(404)
   }
 })
 
-app.put('/movies/:imdbID', function(req, res) {
+app.put('/movies/:imdbID', function (req, res) {
 
   const id = req.params.imdbID
   const exists = id in movieModel
 
   movieModel[req.params.imdbID] = req.body;
-  
+
   if (!exists) {
     res.status(201)
     res.send(req.body)
   } else {
     res.sendStatus(200)
   }
-  
+
 })
 
 app.listen(3000)
